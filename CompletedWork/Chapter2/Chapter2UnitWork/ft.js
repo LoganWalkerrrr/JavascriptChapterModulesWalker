8da8b3390eca3ae5d2206c61a9066a4729baa5ce
Walker <logan.walker@stu.lee.kyschools.us>
2022-02-07 12:41:14 -0500

    Unit 2 Work Finished

---
+++ CompletedWork/Chapter2/Chapter2UnitWork/ft.js
@@ -0,0 +1,23 @@
+/*    JavaScript 6th Edition
+ *    Chapter 2
+ *    Chapter case
+
+ *    Fan Trick Fine Art Photography
+ *    Variables and functions
+ *    Author: Logan Walker
+ *    Date: 02/02/22
+
+ *    Filename: ft.js
+ */
+	//sets all form field vaules to default
+	function resetForm(){
+		document.getElementById("photognum").value = 1;
+		document.getElementById("photoghrs").value = 2;
+		document.getElementById("membook").checked = false;
+		document.getElementById("reprodrights").checked = false;
+		document.getElementById("distance").value = 0;
+
+}
+
+
+	
