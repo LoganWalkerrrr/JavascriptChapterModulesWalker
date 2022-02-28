7df4707bca6b4e1749e50cb60e1060b76c137ee0
Walker <logan.walker@stu.lee.kyschools.us>
2022-02-28 12:36:15 -0500

    Chapter 3-1 done

--- CompletedWork/Chapter2/Chapter2UnitWork/ft.js
+++ CompletedWork/Chapter2/Chapter2UnitWork/ft.js
@@ -9,15 +9,46 @@
 
  *    Filename: ft.js
  */
-	//sets all form field vaules to default
-	function resetForm(){
-		document.getElementById("photognum").value = 1;
-		document.getElementById("photoghrs").value = 2;
-		document.getElementById("membook").checked = false;
-		document.getElementById("reprodrights").checked = false;
-		document.getElementById("distance").value = 0;
 
+
+
+
+//sets all form field vaules to default
+	var photographerCost = 0;
+	var totalCost = 0;
+	var memoryBook = false;
+	var reproductionRights = false;
+
+function calcStaff() {
+var num = document.getElementById("photognum");
+var hrs = document.getElementById("photoghrs");
+totalCost -= photographerCost;
+photographerCost = num.value * 100 * hrs.value;
+totalCost += photographerCost;
+document.getElementById("estimate").innerHTML = "$" +
+totalCost;
 }
 
 
-	
+function resetForm(){
+	document.getElementById("photognum").value = 1;
+	document.getElementById("photoghrs").value = 2;
+	document.getElementById("membook").checked = memoryBook;
+	document.getElementById("reprodrights").checked = reproductionRights;
+	document.getElementById("distance").value = 0;
+
+calcStaff();
+createEventListeners();
+}
+
+
+document.addEventListener("load", resetForm, false);
+
+// creates event listeners
+
+function createEventListeners() {
+
+	document.getElementById("photognum").addEventListener("change", calcStaff, false);
+	document.getElementById("photoghrs").addEventListener("change", calcStaff, false);
+}
+
